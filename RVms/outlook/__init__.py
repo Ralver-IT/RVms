82acaaddcd8f520eb45bb114db5b19e80a0eda52
Ralph <102580725+RV-98@users.noreply.github.com>
2026-01-16 07:02:06 +0100

    Update __init__.py

--- RVms/outlook/__init__.py
+++ RVms/outlook/__init__.py
@@ -2,5 +2,15 @@ from .message import MailMessage
 from .compose import ComposeMessage
 from .attachment import Attachment
 from .address import EmailAddress
+from .client import MailClient
+from .subscriptions import Subscription, SubscriptionClient
 
-__all__ = ["MailMessage", "Attachment", "EmailAddress", "ComposeMessage"]
+__all__ = [
+    "MailMessage",
+    "Attachment",
+    "EmailAddress",
+    "ComposeMessage",
+    "MailClient",
+    "Subscription",
+    "SubscriptionClient"
+]
